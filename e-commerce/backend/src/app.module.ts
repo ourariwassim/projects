9f428d477c8097d10da04b50399641fc93b1d38b
wassourari <wassourari7@gmail.com>
2026-04-13 11:00:05 +0100

    first commit

---
+++ e-commerce/backend/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { dbConfig } from './db/db.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => dbConfig(configService),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ e-commerce/backend/src/db/db.config.ts
@@ -0,0 +1,14 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'process';
+
+export const dbConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'postgres',
+  password: configService.getOrThrow('DB_PASSWORD'),
+  port: configService.getOrThrow('DB_PORT'),
+  username: configService.getOrThrow('DB_USER'),
+  host: configService.getOrThrow('DB_HOST'),
+  database: configService.getOrThrow('DB_NAME'),
+});
